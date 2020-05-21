93b3c7130fff5127c71860f14b2c2eff45b3f973
virferox <jonathan.henin@gmail.com>
2020-05-21 14:54:47 -0400

    Update

--- Scripts/Activate.ps1
+++ Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\jonat\Documents\Masters\TrainYourOwnYOLO"
+$env:VIRTUAL_ENV="C:\Users\jonat\Documents\Masters\TrainYourOwnYOLO\3_Inference\YOLOgpu"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
@@ -35,7 +35,7 @@ if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     function global:_OLD_VIRTUAL_PROMPT {""}
     copy-item function:prompt function:_OLD_VIRTUAL_PROMPT
     function global:prompt {
-        Write-Host -NoNewline -ForegroundColor Green '(TrainYourOwnYOLO) '
+        Write-Host -NoNewline -ForegroundColor Green '(YOLOgpu) '
         _OLD_VIRTUAL_PROMPT
     }
 }
